080fcfea5576c5496d376221dad88b392ee027f0
perrychen901105 <chenzhipeng901105@gmail.com>
2015-02-12 22:07:37 +0800

    add stroke line

--- Flo/Flo/PushButtonView.swift
+++ Flo/Flo/PushButtonView.swift
@@ -31,6 +31,26 @@ class PushButtonView: UIButton {
         
         // set the path's line width to the height of the stroke
         plusPath.lineWidth = plusHeight
+        
+        // move the initial point of the path
+        // to the start of the horizontal stroke
+        plusPath.moveToPoint(CGPoint(x: bounds.width/2 - plusWidth/2 + 0.5, y: bounds.height/2 + 0.5))
+        
+        // add a point to the path at the end of the stroke
+        plusPath.addLineToPoint(CGPoint(x: bounds.width/2 + plusWidth/2 + 0.5, y: bounds.height/2 + 0.5))
+        
+        // vertical line
+        // move to the start of the vertical stroke
+        plusPath.moveToPoint(CGPoint(x: bounds.width/2 + 0.5, y: bounds.height/2 - plusWidth/2 + 0.5))
+        
+        // add the end point to the vertical stroke
+        plusPath.addLineToPoint(CGPoint(x: bounds.width/2 + 0.5, y: bounds.height/2 + plusWidth/2 + 0.5))
+        
+        // set the stroke color
+        UIColor.whiteColor().setStroke()
+        
+        // draw the stroke
+        plusPath.stroke()
     }
 
 
