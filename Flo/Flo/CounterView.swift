f482cb1dff41624e41a743ad3a2c95850efb832d
perrychen901105 <chenzhipeng901105@gmail.com>
2015-02-20 19:36:55 +0800

    finish 2nd part

--- Flo/Flo/CounterView.swift
+++ Flo/Flo/CounterView.swift
@@ -92,6 +92,41 @@ let π:CGFloat = CGFloat(M_PI)
         outlineColor.setStroke()
         outlinePath.lineWidth = 5.0
         outlinePath.stroke()
+        
+        // Counter View Markers
+        let context = UIGraphicsGetCurrentContext()
+        
+        // 1 - save original state
+        CGContextSaveGState(context)
+        outlineColor.setFill()
+        
+        let markerWidth:CGFloat = 5.0
+        let markerSize:CGFloat = 10.0
+        
+        // 2 - the marker rectangle positioned ata the top left
+        var markerPath = UIBezierPath(rect: CGRect(x: -markerWidth/2, y: 0, width: markerWidth, height: markerSize))
+        
+        // 3 - move top left of context to the previous center position
+        CGContextTranslateCTM(context, rect.width/2, rect.height/2)
+        
+        for i in 1...NoOfGlasses {
+            // 4 - save the centred context
+            CGContextSaveGState(context)
+            
+            // 5 - calculate the rotation angle
+            var angle = arcLengthPerGlass * CGFloat(i) + startAngle - π/2
+            
+            // rotate and translate
+            CGContextRotateCTM(context, angle)
+            CGContextTranslateCTM(context, 0, rect.height/2 - markerSize)
+
+            // 6 - fill the marker rectangle
+            markerPath.fill()
+
+            // 7 - restore the centerd context for the next rotate
+            CGContextRestoreGState(context)
+        }
+        CGContextRestoreGState(context)
     }
     
 
--- Flo/Flo/ViewController.swift
+++ Flo/Flo/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var graphView: GraphView!
     
+    //Label outlets
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
     var isGraphViewShowing = false
     
     @IBAction func counterViewTap(gesture: UITapGestureRecognizer?) {
@@ -32,6 +36,7 @@ class ViewController: UIViewController {
                 duration: 1.0,
                 options: .TransitionFlipFromRight | .ShowHideTransitionViews,
                 completion: nil)
+            setupGraphDisplay()
         }
         isGraphViewShowing = !isGraphViewShowing
     }
@@ -48,6 +53,49 @@ class ViewController: UIViewController {
         counterLabel.text = String(counterView.counter)
         if isGraphViewShowing {
             counterViewTap(nil)
+        }
+    }
+    
+    func setupGraphDisplay() {
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays:Int = 7
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(maxElement(graphView.graphPoints))"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        // set up labels
+        // day of week labels are set up in storyboard with tags
+        // today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+        let dateFormatter = NSDateFormatter()
+        let calendar = NSCalendar.currentCalendar()
+        let componentOptions:NSCalendarUnit = .WeekdayCalendarUnit
+        let components = calendar.components(componentOptions, fromDate: NSDate())
+        
+        var weekday = components.weekday
+        
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+        //5 - set up the day name labels with correct day
+        for i in reverse(1...days.count) {
+            let labelView = graphView.viewWithTag(i) as UILabel
+            if weekday == 7 {
+                weekday = 0
+            }
+            labelView.text = days[weekday--]
+            if weekday < 0 {
+                weekday = days.count - 1
+            }
         }
     }
     
